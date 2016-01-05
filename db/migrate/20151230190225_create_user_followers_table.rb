97bdf8cd36d9fbb19cc77bb44e7d7b2254401244
Richard Mable <richardbmable@gmail.com>
2016-01-05 15:37:15 -0500

    removed /db/migrate/20151230190225_create_user_followers_table as it was also throwing errors

--- db/migrate/20151230190225_create_user_followers_table.rb
+++
@@ -1,7 +0,0 @@
-class CreateUserFollowersTable < ActiveRecord::Migration
-  def change
-  	create_table :user_followers do |uf|
-  		uf.integer :user_id
-  		uf.integer :user_follower_id
-  end
-end
